9de9706d97794abe26d1ef573d03912be800c803
Sai Vikram Patnaik <56348644+Vikram126@users.noreply.github.com>
2020-03-28 21:42:12 +0530

    Update prioritySch.c
    # Added Table(arrival time, burst time, waiting time)

--- prioritySch.c
+++ prioritySch.c
@@ -2,9 +2,10 @@
 #include<stdbool.h>
 // Process Blueprint.
 struct process { 
-    int pid; //Process Id.
-    int arrival_time;//the time when the process arrives for execution.
+    	int pid; //Process Id.
+    	int arrival_time;//the time when the process arrives for execution.
 	int burst_time;//estimated run time.
+	int wait_time;//0 as default. calculated as arrival time - burst time
     bool isCompleted;//to check if the process has been executed or not
 };
 
@@ -13,14 +14,13 @@ void main()
 { 
     int i, j, t;//two loop variables and integer temporary variable respectively.
 	int sum_bt = 0;//to hold the sum of burst times
-  	
 	int size=5;//size of the array
-  	//Process Array format: {pid,arrival time,burst time,isCompleted}
-	struct process p[]={{0,0,3,false},
-						{1,2,6,false},
-						{2,4,4,false},
-						{3,6,5,false},
-						{4,8,2,false}};
+  	//Process Array format: {pid,arrival time,burst time,wait time,isCompleted}
+	struct process p[]={{0,0,3,0,false},
+						{1,2,6,0,false},
+						{2,4,4,0,false},
+						{3,6,5,0,false},
+						{4,8,2,0,false}};
     struct process temporary;//for holding a process temporarily.
     
 	
@@ -43,6 +43,8 @@ void main()
             } 
         } 
     } 
+    
+    printf("Order :\t");
     
     for (t = p[0].arrival_time; t < sum_bt;) { 
   
@@ -68,7 +70,19 @@ void main()
         t += p[loc].burst_time; 
         // Updating Completion Status 
         p[loc].isCompleted = true;
+         // Calculation of waiting time 
+        p[loc].wait_time = t - p[loc].arrival_time - p[loc].burst_time; 
         //Printing Order
-        printf("p%d ",p[loc].pid);
+        printf("p%d\t====>\t ",p[loc].pid);
+    }
+    printf("Completed");
+	//Table representing the gantt chart of the scheduling
+    printf("\n\n\tTable to visualsise the Gantt Chart");
+    printf("\n\nPid\tArrival Time\tBurst Time\tWaiting Time"); 
+    for(i=0;i<size;i++)
+    {
+	
+        printf("\np%i\t\t%d\t\t", p[i].pid, p[i].arrival_time); 
+        printf("%d\t\t%d\t\t", p[i].burst_time, p[i].wait_time);
     }
 } 
